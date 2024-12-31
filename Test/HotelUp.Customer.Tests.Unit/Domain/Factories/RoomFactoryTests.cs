18a1023391cb6a934537f37853145473668bf345
Wiaz24 <wiazhub@gmail.com>
2024-12-31 14:18:18 +0100

    RoomFactory tests

---
+++ Test/HotelUp.Customer.Tests.Unit/Domain/Factories/RoomFactoryTests.cs
@@ -0,0 +1,61 @@
+using HotelUp.Customer.Domain.Consts;
+using HotelUp.Customer.Domain.Factories;
+using HotelUp.Customer.Domain.Factories.Exceptions;
+using HotelUp.Customer.Domain.ValueObjects;
+using HotelUp.Customer.Unit.Domain.Repositories;
+using Shouldly;
+
+namespace HotelUp.Customer.Unit.Domain.Factories;
+
+public class RoomFactoryTests
+{
+    [Fact]
+    public async Task Create_IfRoomDoesNotExists_ShouldCreateRoom()
+    {
+        // Arrange
+        var roomRepository = new TestRoomRepository();
+        var roomFactory = new RoomFactory(roomRepository);
+        
+        // Act
+        var exception = await Record.ExceptionAsync(() => roomFactory
+            .Create(1, 
+                2, 
+                1, 
+                false, 
+                RoomType.Basic, 
+                new ImageUrl("http://www.yourserver.com/logo.png")));
+        
+        // Assert
+        exception.ShouldBeNull();
+        
+    }
+    
+    [Fact]
+    public async Task Create_IfRoomExists_ThrowsRoomAlreadyExistsException()
+    {
+        // Arrange
+        var roomRepository = new TestRoomRepository();
+        var roomFactory = new RoomFactory(roomRepository);
+        var firstRoom = await roomFactory
+            .Create(1,
+                2,
+                1,
+                false,
+                RoomType.Basic,
+                new ImageUrl("http://www.yourserver.com/logo.png"));
+        roomRepository.Rooms.Add(firstRoom.Id, firstRoom);
+            
+        // Act
+        var exception = await Record.ExceptionAsync(() => roomFactory
+            .Create(1, 
+                4, 
+                2, 
+                true, 
+                RoomType.Basic, 
+                new ImageUrl("http://www.yourserver.com/otherlogo.png")));
+        
+        //Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<RoomAlreadyExistsException>();
+    }
+}
---
+++ Test/HotelUp.Customer.Tests.Unit/Domain/Repositories/TestRoomRepository.cs
@@ -0,0 +1,20 @@
+using HotelUp.Customer.Domain.Entities;
+using HotelUp.Customer.Domain.Repositories;
+using HotelUp.Customer.Domain.ValueObjects;
+
+namespace HotelUp.Customer.Unit.Domain.Repositories;
+
+internal class TestRoomRepository : IRoomRepository
+{
+    public Dictionary<Guid, Reservation> Reservations = new();
+    public Dictionary<int, Room> Rooms = new();
+    public Task<IEnumerable<Room>> GetAvailableRoomsAsync(ReservationPeriod period)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Room?> GetRoomAsync(int number)
+    {
+        return Task.FromResult(Rooms.TryGetValue(number, out var room) ? room : null);
+    }
+}
