82f224ce10b4c59cdbbcdb1444cf01688d5b4ddd
Wiaz <wiazhub@gmail.com>
2025-01-30 11:51:03 +0100

    k8s yaml and masstransit name formatter changed

--- Shared/HotelUp.Customer.Shared/Messaging/Extensions.cs
+++ Shared/HotelUp.Customer.Shared/Messaging/Extensions.cs
@@ -16,7 +16,7 @@ internal static class Extensions
         services.AddRabbitMq();
         services.AddMassTransit(busConfigurator =>
         {
-            busConfigurator.SetKebabCaseEndpointNameFormatter();
+            busConfigurator.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter(prefix: "customer"));
             if (assembliesWithConsumers.Length > 0)
             {
                 busConfigurator.AddConsumers(assembliesWithConsumers);
